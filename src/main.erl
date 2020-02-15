83de50cf7360a7c3bdc76fe3aacc096d378dc742
katandser <Gtr132rew_>
2020-02-15 08:19:40 +0300

    init

---
+++ src/main.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%%% @author vemce
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. февр. 2020 6:34
+%%%-------------------------------------------------------------------
+-module(main).
+-include_lib("eunit/include/eunit.hrl").
+-author("vemce").
+
+%% API
+%%-export([main/0]).
+
+swap([]) -> [];
+swap([H1, H2 | T]) when H1 > H2 -> [H2 | swap([H1 | T])];
+swap([H | T]) -> [H | swap(T)].
+
+%%main() ->
+%%  erlang:display(swap([1,3,2,1,5,1,2,3])).
+
+det_test() -> [
+
+  ?_assertEqual(swap([3,2,1]), [1,2,3])
+].
