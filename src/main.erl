2eb72b75ad0d75c595b7dc95ed3d5cbf151483c6
katandser <Gtr132rew_>
2020-02-15 09:14:12 +0300

    add full sort? with test

--- src/main.erl
+++ src/main.erl
@@ -10,19 +10,26 @@
 -include_lib("eunit/include/eunit.hrl").
 -author("vemce").
 
-%% API
-%%-export([main/0]).
 
 swap([]) -> [];
 swap([H1, H2 | T]) when H1 > H2 -> [H2 | swap([H1 | T])];
 swap([H | T]) -> [H | swap(T)].
 
-%%main() ->
-%%  erlang:display(swap([1,3,2,1,5,1,2,3])).
+sort(X, L)  when L > 0 -> sort(swap(X), L - 1);
+sort(X, L) -> X.
+sort(X) -> sort(X, lists:flatlength(X)).
+
 
 det_test() -> [
 
   ?assertEqual(swap([3,2,1]), [2,1,3]),
   ?assertEqual(swap([5,6,1]), [5,1,6]),
-  ?assertEqual(swap([3,2,5]), [2,3,5])
+  ?assertEqual(swap([3,2,5]), [2,3,5]),
+
+  ?assertEqual(sort([3,2,1]), [1,2,3]),
+  ?assertEqual(sort([3,2,1,0,0]), [0,0,1,2,3]),
+  ?assertEqual(sort([1,2,3]), [1,2,3]),
+  ?assertEqual(sort([3,3,4]), [3,3,4]),
+  ?assertEqual(sort([8,1,3,5]), [1,3,5,8]),
+  ?assertEqual(sort([-100,-1000,-10000]), [-10000,-1000,-100])
 ].
